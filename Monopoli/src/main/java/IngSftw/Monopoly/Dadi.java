e9ac12b72870ac613de37336c7bfcd5a7a87ee6b
alorenzi10 <127225142+alorenzi10@users.noreply.github.com>
2024-10-16 16:24:46 +0200

    Classe dadi

--- Monopoli/src/main/java/IngSftw/Monopoly/Dadi.java
+++ Monopoli/src/main/java/IngSftw/Monopoly/Dadi.java
@@ -1,30 +1,37 @@
 package IngSftw.Monopoly;
 
-import java.util.*;
-import java.time.*;
+import java.util.Random;
 
 public class Dadi {
+	
+	private int dado1;
+	private int dado2;
+	private Random random;
 
-    private final int NUM_DICE = 2;
-    private int[] dice;
+    public Dadi() {
+    	random=new Random();
+    }
 
     public void roll() {
-
+    	dado1=random.nextInt(6)+1;
+    	dado2=random.nextInt(6)+1;
     }
-
-    public int getDice() {
-    	
-    }
-
+    
     public int getTotal() {
-    	
+    	return dado1+dado2;
     }
-
+    
+    public int getDado1() {
+    	return dado1;
+    }
+    
+    public int getDado2() {
+    	return dado2;
+    }
+  
     public boolean isDouble() {
-
+    	return dado1==dado2;
     }
-
-    public String toString() {
-
-    }
+    
+    
 }
